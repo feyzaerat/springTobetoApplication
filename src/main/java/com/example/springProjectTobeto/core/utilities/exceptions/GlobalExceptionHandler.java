46929bdc27e70b3f28d91cd47edcd9a566a889f6
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-09 04:51:53 +0300

    fixed RestController annotation as RestControllerAdvice

--- src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -3,13 +3,10 @@ package com.example.springProjectTobeto.core.utilities.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@ControllerAdvice
+@RestControllerAdvice
+
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({RuntimeException.class})
