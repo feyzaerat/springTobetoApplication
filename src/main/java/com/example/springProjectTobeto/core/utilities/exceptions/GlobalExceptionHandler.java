6e522e9ced62f162733e379c14446d6248330445
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-09 04:34:33 +0300

    Return message error was fixed by adding @ControllerAdvice annotation??

--- src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,22 @@
 package com.example.springProjectTobeto.core.utilities.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleRuntimeError(RuntimeException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleValidationException(){
