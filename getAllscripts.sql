cd8f70f058c039a3436617de670acf886d60cf2e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-09 04:26:05 +0300

    .

---
+++ getAllscripts.sql
@@ -0,0 +1,26 @@
+SELECT * FROM public.bills
+ORDER BY id ASC ;
+
+SELECT * FROM public.brands
+ORDER BY id ASC ;
+
+SELECT * FROM public.cars
+ORDER BY id ASC ;
+
+SELECT * FROM public.customers
+ORDER BY id ASC ;
+
+SELECT * FROM public.companies
+ORDER BY id ASC ;
+
+SELECT * FROM public.departments
+ORDER BY id ASC ;
+
+SELECT * FROM public.employees
+ORDER BY id ASC ;
+
+SELECT * FROM public.orders
+ORDER BY id ASC ;
+
+SELECT * FROM public.policies
+ORDER BY id ASC ;
--- src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.example.springProjectTobeto.core.utilities.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@ControllerAdvice
 
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({RuntimeException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleRuntimeError(RuntimeException exception){
-        return exception.getMessage();
+    public ResponseEntity<String> handleRuntimeError(RuntimeException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
