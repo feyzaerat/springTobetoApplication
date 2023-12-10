dce018f965f4f2995eb4cf9e43bb4a5ae10c1f82
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-11 01:52:48 +0300

    reformatted files in core package

--- src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springProjectTobeto/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.example.springProjectTobeto.core.utilities.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 
@@ -11,12 +13,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({RuntimeException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleRuntimeError(RuntimeException exception){
+    public ResponseEntity<String> handleRuntimeError(RuntimeException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationException(){
+    public String handleValidationException() {
         return "Validation Error";
     }
 }
